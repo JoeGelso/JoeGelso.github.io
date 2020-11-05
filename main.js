c9b6f2f8b466d778ea0018d5145cf51e649e6f7b
JoeGelso <62577678+JoeGelso@users.noreply.github.com>
2020-11-05 16:17:23 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top -100
+        }, 2000);
+    })
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    });
+});
---
+++ readme.txt
@@ -0,0 +1,2 @@
+This website serves as my personal portfolio. I used the Animate on scroll library to animate various sections of this page. 
+https://github.com/michalsnik/aos
